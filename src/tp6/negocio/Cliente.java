3b48b5439294b46819a2985613479da7f033b99a
juliettegigi <mar9ina@gmail.com>
2023-05-14 18:07:24 -0300

    ultimoc

--- src/tp6/negocio/Cliente.java
+++ src/tp6/negocio/Cliente.java
@@ -56,6 +56,11 @@ public class Cliente {
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + ", ciudad=" + ciudad + ", direccion=" + direccion + '}';
+    }
     
     
 }
